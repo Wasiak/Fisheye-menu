f22b55e496a3b2d97de91dd4f887a50799b38723
Kuba Wasiak <qbawasiak@wp.pl>
2016-03-24 00:15:07 +0100

    mechanics works

--- main.js
+++ main.js
@@ -2,39 +2,44 @@ var items = $('#main-menu li');
 
 var item = items[0];
 var lastItem = items[items.length - 1];
+var itemsMiddle = [];
 
 var topLimit = item.offsetTop - 10;
 var bottomLimit = item.offsetTop + item.offsetHeight + 10;
-var leftLimit = item.offsetLeft - (item.offsetWidth * 2);
-var rightLimit = lastItem.offsetLeft + (lastItem.offsetWidth * 3);
+var itemWidth = item.offsetWidth;
+var growRatio = 1.8;
+var sizeDiff = Math.round(itemWidth * (growRatio - 1));
 
-var getItemOffsets = function(item) {
-  var offsets = [
-    item.offsetLeft,
-    item.offsetLeft + item.offsetWidth
-  ];
-  return offsets;
+var getItemMiddle = function(item) {
+  var point = item.offsetLeft + (item.offsetWidth / 2);
+  return point;
 };
 
-var makeItemBigger = function(x, y) {
-  // console.log('run makeItemBigger');
+var getAllItemsMiddle = function() {
   $.each(items, function(index, item) {
-    // console.log(index, item);
-    var offsets = getItemOffsets(item);
-    // console.log(offsets);
-    console.log(x, y);
-    
+    var middle = getItemMiddle(item);
+    itemsMiddle.push(middle);
+  });
+}();
+
+var makeItemBigger = function(x, y) {
+  var verticallyCorrect = (y > topLimit && y < bottomLimit) ? true : false;
+  $.each(items, function(index, item) {
+    var middlePoint = itemsMiddle[index];
+    var limit = item.offsetWidth * 1.5;
+    var dist = Math.abs(x - middlePoint);
+    var ratio = (dist > limit) ? 1 : (dist / limit);
+    ratio = verticallyCorrect ? ratio : 1;
+    var newSize = itemWidth + ((1 - ratio) * sizeDiff);
+    // item.css({
+    //   'width': newSize + 'px'
+    // });
+    item.style.width = newSize + 'px';
   });
 };
 
 $('body').mousemove(function(e) {
-  $('#x').text(e.pageX);
-  $('#y').text(e.pageY);
   var pointerX = e.pageX;
   var pointerY = e.pageY;
-  if (pointerY > topLimit && pointerY < bottomLimit && 
-      pointerX > leftLimit && pointerX < rightLimit) {
-    makeItemBigger(pointerX, pointerY);
-  }
-
+  makeItemBigger(pointerX, pointerY);
 });
