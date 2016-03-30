be7059e62c0328a96f2b2870cdc549750af0517e
Kuba Wasiak <qbawasiak@wp.pl>
2016-03-31 00:32:34 +0200

    on move out vert limit transition

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ var fisheyeMenu = function(options) {
   // get all items from fisheye-menu
   var items = Array.prototype.slice.call(document.querySelectorAll('.fisheye-menu li'));
   var item = items[0];
-  var staticItem = item;
   //get horizontal center point of each item
   var itemsMiddle = items.map(function(item){ return item.offsetLeft + (item.offsetWidth / 2); });
-  var itemsVertMiddle = items.map(function(item){ return item.offsetTop + (item.offsetHeight / 2); });
+
+  // var itemsVertMiddle = items.map(function(item){ return item.offsetTop + (item.offsetHeight / 2); });
   var itemWidth = item.offsetWidth;
   // vertical limits of function running
   var topLimit = item.offsetTop - (options.verticalLimit || 10);
@@ -14,16 +14,17 @@ var fisheyeMenu = function(options) {
   var growRatio = options.growRatio || 1.8;
   // horizontal limit of grow reaction for single item
   var limit = item.offsetWidth * (options.horizontalLimit || 1.5);
-  var vertLimit = item.offsetHeight;
+  
+  // var vertLimit = item.offsetHeight;
   var sizeDiff = Math.round(itemWidth * (growRatio - 1));
 
   var makeItemBigger = function(x, y) {
     var verticallyCorrect = (y > topLimit && y < bottomLimit) ? true : false;
-    if (!verticallyCorrect) {
-        staticItem.parentNode.classList.add('go-down');
-      } else {
-        staticItem.parentNode.classList.remove('go-down');
-      }
+    if (!verticallyCorrect && !item.parentNode.classList.contains('go-down')) {
+        item.parentNode.classList.add('go-down');
+    } else if (verticallyCorrect && item.parentNode.classList.contains('go-down')) {
+      item.parentNode.classList.remove('go-down');
+    }
 
     items.forEach(function(item, index) { 
       var middlePoint = itemsMiddle[index];
